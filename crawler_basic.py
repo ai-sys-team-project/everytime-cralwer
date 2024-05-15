c4685c65db17b87f5d68c3ee1ae1dceaecf34777
sangji <sangllee20@gmail.com>
2024-05-15 20:22:20 +0900

    update

--- crawler_basic.py
+++ crawler_basic.py
@@ -25,11 +25,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 ID = 'seinundzeit1'
 PWD = 's9hkfm$w'
 
-BOARD_NUMBER = '375132' # 성대 새내기 게시판
+BOARD_NUMBER = '370444' # 인사캠 자유 게시판
 BASE_URL = f'https://everytime.kr/{BOARD_NUMBER}'
 
-PAGE_FROM = 1401 #  # 101, 1001
-PAGE_TO = 1800
+PAGE_FROM = 1 #  # 101, 1001
+PAGE_TO = 50
 
 BREAK_DATE = '23/05/08'
 TODAY = datetime.today().strftime('%m/%d')
@@ -37,15 +37,6 @@ TODAY = datetime.today().strftime('%m/%d')
 driver = webdriver.Chrome(service= Service(ChromeDriverManager().install()))
 context = ssl._create_unverified_context()            
 
-
-def save_articles_to_csv(articles):
-    with open('articles.csv', 'w', newline='') as csvfile:
-        fieldnames = ['title', 'contents', 'likes', 'scrapes', 'comments']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for article in articles:
-            writer.writerow(article)
 
 def save_articles_to_xlsx(articles):
     df = pd.DataFrame(articles)
@@ -93,66 +84,71 @@ def crawler(base_url=BASE_URL, id=ID, pwd=PWD, break_date=BREAK_DATE, today=TODA
     comments = []
     likes, scrapes = 0, 0
     
+    try:
+        for page_num in range(page_from, page_to+1): # 101, 1001 # 새내기: 1 ~ 1840까지가 1년치 
+            page_url = f"{base_url}/p/{page_num}"
+            driver.get(page_url)
+            driver.implicitly_wait(3)
+            sleep(2)
 
-    for page_num in range(page_from, page_to+1): # 101, 1001 # 새내기: 1 ~ 1840까지가 1년치 
-        page_url = f"{base_url}/p/{page_num}"
-        driver.get(page_url)
-        driver.implicitly_wait(3)
-        sleep(2)
+            articles = driver.find_elements(By.CSS_SELECTOR, '.wrap.articles article.list')
 
-        articles = driver.find_elements(By.CSS_SELECTOR, '.wrap.articles article.list')
+            time_elements = driver.find_elements(By.CSS_SELECTOR, 'div.info time.small')
+            print('time_elements: ', time_elements[0].text.split(' ')[0])
+            if time_elements[0].text.split(' ')[0] == break_date:
+                break
+            else: 
+                pass
 
-        time_elements = driver.find_elements(By.CSS_SELECTOR, 'div.info time.small')
-        print('time_elements: ', time_elements[0].text.split(' ')[0])
-        if time_elements[0].text.split(' ')[0] == break_date:
-            break
-        else: 
-            pass
+            for article in articles:
+                time_element = article.find_element(By.CSS_SELECTOR, 'div.info time.small')
+                date = time_element.text.split(' ')[0]
+                
+                if '/' not in date:
+                    date = today
+                if date == break_date:
+                    break
 
-        for article in articles:
-            time_element = article.find_element(By.CSS_SELECTOR, 'div.info time.small')
+                times.append(date)
+
+                href = article.find_element(By.CSS_SELECTOR, 'a.article').get_attribute('href')
+                hrefs.append(href)
+
+
+        for href in hrefs:
+            driver.get(href)
+            sleep(2)
+        
+            time_element = driver.find_element(By.XPATH, '//time[@class="large"]')
             date = time_element.text.split(' ')[0]
+
+            if date == break_date:
+                break
             
             if '/' not in date:
                 date = today
-            if date == break_date:
-                break
 
-            times.append(date)
+            title = driver.find_element(By.CSS_SELECTOR, 'h2.large').text
+            contents = driver.find_element(By.CSS_SELECTOR, 'a.article p.large').text
 
-            href = article.find_element(By.CSS_SELECTOR, 'a.article').get_attribute('href')
-            hrefs.append(href)
-
-
-    for href in hrefs:
-        driver.get(href)
-        driver.implicitly_wait(3)
-
-        time_element = driver.find_element(By.XPATH, '//time[@class="large"]')
-        date = time_element.text.split(' ')[0]
-
-        if date == break_date:
-            break
+            likes = int(driver.find_element(By.CSS_SELECTOR, 'li.vote').text)
+            scrapes = int(driver.find_element(By.CSS_SELECTOR, 'li.scrap').text) 
+            comments = extract_comments(driver.find_element(By.CSS_SELECTOR, 'div.comments'))
+            
+            article = { 
+                'date': date,
+                'title': title,
+                'contents': contents,
+                'likes': likes,
+                'scrapes': scrapes,
+                'comments': comments
+            }
+            articles_data.append(article)
         
-        if '/' not in date:
-            date = today
+    except selenium.common.exceptions.NoSuchElementException as e:
+        print(f"Error finding element: {e}")
+        save_articles_to_xlsx(articles_data)
 
-        title = driver.find_element(By.CSS_SELECTOR, 'h2.large').text
-        contents = driver.find_element(By.CSS_SELECTOR, 'a.article p.large').text
-
-        likes = int(driver.find_element(By.CSS_SELECTOR, 'li.vote').text)
-        scrapes = int(driver.find_element(By.CSS_SELECTOR, 'li.scrap').text) 
-        comments = extract_comments(driver.find_element(By.CSS_SELECTOR, 'div.comments'))
-        
-        article = { 
-            'date': date,
-            'title': title,
-            'contents': contents,
-            'likes': likes,
-            'scrapes': scrapes,
-            'comments': comments
-        }
-        articles_data.append(article)
 
     # save_articles_to_csv(articles_data)
     save_articles_to_xlsx(articles_data)
