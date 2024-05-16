ec6e54192db82630ea201adfbc415a76953aa890
sangji <sangllee20@gmail.com>
2024-05-16 09:08:59 +0900

    update

--- crawler_basic.py
+++ crawler_basic.py
@@ -19,6 +19,7 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+import selenium.common.exceptions  # selenium 모듈 임포트
 
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -28,8 +29,8 @@ PWD = 's9hkfm$w'
 BOARD_NUMBER = '370444' # 인사캠 자유 게시판
 BASE_URL = f'https://everytime.kr/{BOARD_NUMBER}'
 
-PAGE_FROM = 1 #  # 101, 1001
-PAGE_TO = 50
+PAGE_FROM = 101 #  # 101, 1001
+PAGE_TO = 300
 
 BREAK_DATE = '23/05/08'
 TODAY = datetime.today().strftime('%m/%d')
@@ -38,9 +39,13 @@ driver = webdriver.Chrome(service= Service(ChromeDriverManager().install()))
 context = ssl._create_unverified_context()            
 
 
-def save_articles_to_xlsx(articles):
-    df = pd.DataFrame(articles)
-    df.to_excel(f'{BOARD_NUMBER}_{PAGE_FROM}-{PAGE_TO}.xlsx', index=False)
+# def save_articles_to_xlsx(articles):
+#     df = pd.DataFrame(articles)
+#     df.to_excel(f'{BOARD_NUMBER}_{PAGE_FROM}-{PAGE_TO}.xlsx', index=False)
+
+def save_articles_to_xlsx(articles_data, file_name):
+    df = pd.DataFrame(articles_data)
+    df.to_excel(file_name, index=False)
 
 def extract_comments(div_comments):
     comments = []
@@ -118,40 +123,49 @@ def crawler(base_url=BASE_URL, id=ID, pwd=PWD, break_date=BREAK_DATE, today=TODA
         for href in hrefs:
             driver.get(href)
             sleep(2)
-        
-            time_element = driver.find_element(By.XPATH, '//time[@class="large"]')
-            date = time_element.text.split(' ')[0]
 
-            if date == break_date:
+            try: 
+                time_element = driver.find_element(By.XPATH, '//time[@class="large"]')
+                date = time_element.text.split(' ')[0]
+
+                if date == break_date:
+                    break
+                
+                if '/' not in date:
+                    date = today
+
+                title = driver.find_element(By.CSS_SELECTOR, 'h2.large').text
+                contents = driver.find_element(By.CSS_SELECTOR, 'a.article p.large').text
+
+                likes = int(driver.find_element(By.CSS_SELECTOR, 'li.vote').text)
+                scrapes = int(driver.find_element(By.CSS_SELECTOR, 'li.scrap').text) 
+                comments = extract_comments(driver.find_element(By.CSS_SELECTOR, 'div.comments'))
+                
+                article = { 
+                    'date': date,
+                    'title': title,
+                    'contents': contents,
+                    'likes': likes,
+                    'scrapes': scrapes,
+                    'comments': comments
+                }
+                articles_data.append(article)
+            
+            except selenium.common.exceptions.NoSuchElementException as e:
+                print(f"Error finding element: {e}")
+                # save_articles_to_xlsx(articles_data)
+                save_articles_to_xlsx(articles_data, f'{BOARD_NUMBER}_until_error.xlsx')
                 break
-            
-            if '/' not in date:
-                date = today
-
-            title = driver.find_element(By.CSS_SELECTOR, 'h2.large').text
-            contents = driver.find_element(By.CSS_SELECTOR, 'a.article p.large').text
-
-            likes = int(driver.find_element(By.CSS_SELECTOR, 'li.vote').text)
-            scrapes = int(driver.find_element(By.CSS_SELECTOR, 'li.scrap').text) 
-            comments = extract_comments(driver.find_element(By.CSS_SELECTOR, 'div.comments'))
-            
-            article = { 
-                'date': date,
-                'title': title,
-                'contents': contents,
-                'likes': likes,
-                'scrapes': scrapes,
-                'comments': comments
-            }
-            articles_data.append(article)
         
     except selenium.common.exceptions.NoSuchElementException as e:
         print(f"Error finding element: {e}")
-        save_articles_to_xlsx(articles_data)
+        # save_articles_to_xlsx(articles_data)
+        save_articles_to_xlsx(articles_data, f'{BOARD_NUMBER}_until_error.xlsx')
 
+    driver.quit()
 
-    # save_articles_to_csv(articles_data)
-    save_articles_to_xlsx(articles_data)
+    # save_articles_to_xlsx(articles_data)
+    save_articles_to_xlsx(articles_data, f'{BOARD_NUMBER}_{PAGE_FROM}-{PAGE_TO}.xlsx')
 
     end_time = datetime.now()
     elapsed_time = end_time - start_time
